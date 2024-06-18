c88976d31aee11b4702bc0f48d58bd0109fc7071
Enrico Granata <granata.enrico@gmail.com>
2024-06-17 21:22:06 -0600

    Rename number to integer in the assembler grammar

--- assembler/src/ast/instructions/push.rs
+++ assembler/src/ast/instructions/push.rs
@@ -13,7 +13,7 @@ pub(crate) fn from_parse_tree(p: pest::iterators::Pair<'_, Rule>) -> AssemblerRe
             .find_first_tagged("idx")
             .expect("need an index");
         let idx: Either<u16, String> = match idx.as_rule() {
-            Rule::number => {
+            Rule::integer => {
                 Either::Left(idx.as_str().parse::<u16>().expect("invalid numeric index"))
             }
             Rule::string => Either::Right(parse_string_trim(idx.as_str())),
